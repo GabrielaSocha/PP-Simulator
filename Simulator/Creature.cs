d0bb9a5f91a7d7279b68373b42b2d7b3e4e2276e
GabrielaSocha <232490@student.uek.krakow.pl>
2024-10-26 15:05:33 +0200

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection.Emit;
+using System.Xml.Linq;
+
+namespace Simulator;
+
+public class Creature
+{
+    public string Name { get; set; }
+    public int Level { get; set; } = 1;
+    public string Info
+    {
+        get { return $"{Name} [{Level}]"; }
+    }
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    }
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature() { }
+}
