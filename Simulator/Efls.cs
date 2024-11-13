66d8d12813c480d1429a98b036f41936562cf890
GabrielaSocha <232490@student.uek.krakow.pl>
2024-11-13 15:40:38 +0100

    Biblioteka

--- Simulator/Efls.cs
+++ Simulator/Efls.cs
@@ -27,6 +27,6 @@ public class Elf : Creature
     }
     public Elf() { }
     public override int Power => 8 * Level + 2 * Agility;
-    public override string Greeting() => $"Hi, I'm {Name}, my level is {Level}, my agility is {Agility}."
+    public override string Greeting() => $"Hi, I'm {Name}, my level is {Level}, my agility is {Agility}.";
     public override string Info => $"{Name} [{Level}][{Agility}]";
 }
