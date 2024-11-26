8905e95c81aff809e93b709eec1bcfc41ac214f2
GabrielaSocha <232490@student.uek.krakow.pl>
2024-11-26 20:43:57 +0100

    Simulation

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -5,63 +5,7 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Point p = new(10, 25);
-        Console.WriteLine(p.Next(Direction.Right));
-        Console.WriteLine(p.NextDiagonal(Direction.Right));
-        Lab5a();
-        Lab5b();
-    }
-
-    static void Lab5a()
-    {
-        var rectangle0 = new Rectangle(new Point(1,0), new Point(4,3));
-        Console.WriteLine(rectangle0.ToString());
-
-        var rectangle = new Rectangle(new Point(4, 3), new Point(1, 0));
-        Console.WriteLine(rectangle.ToString());
-
-        var rectangle1 = new Rectangle(8, 5, 3, 1);
-        Console.WriteLine(rectangle1.ToString());
-
-        try
-        {
-            var rectangle2 = new Rectangle(new Point(0, 8), new Point(0, 5));
-            Console.WriteLine(rectangle2.ToString());
-        }
-        catch (ArgumentException exept)
-        {
-
-            Console.WriteLine(exept.Message);
-        }
-        Console.WriteLine(rectangle0.Contains(new Point(2,1)));
-        Console.WriteLine(rectangle0.Contains(new Point(5,8)));
-    }
-    static void Lab5b()
-    {
-        SmallSquareMap smallSquareMap = new SmallSquareMap(11);
         
-        Point position = new Point(0,0);
-        Console.WriteLine($"Your position: {position}");
-
-        position = smallSquareMap.Next(position, Direction.Up);
-        position = smallSquareMap.NextDiagonal(position, Direction.Up);
-        Console.WriteLine($"Your position: {position}");
-
-        try
-        {
-            SmallSquareMap smallSquareMap1 = new SmallSquareMap(30);
-
-        }
-        catch (ArgumentOutOfRangeException mapSize)
-        {
-
-            Console.WriteLine(mapSize.Message);
-        }
-        for (int i = 0; i < 11; i++)
-        {
-            position = smallSquareMap.NextDiagonal(position, Direction.Up);
-            Console.WriteLine($"Your position: {position}");
-        }
     }
     
 }
---
+++ Simulator/Simulation.cs
@@ -0,0 +1,98 @@
+﻿using Simulator.Maps;
+
+namespace Simulator;
+
+public class Simulation
+{
+    /// <summary>
+    /// Simulation's map.
+    /// </summary>
+    public Map Map { get; }
+
+    /// <summary>
+    /// Creatures moving on the map.
+    /// </summary>
+    public List<Creature> Creatures { get; }
+
+    /// <summary>
+    /// Starting positions of creatures.
+    /// </summary>
+    public List<Point> Positions { get; }
+
+    /// <summary>
+    /// Cyclic list of creatures moves. 
+    /// Bad moves are ignored - use DirectionParser.
+    /// First move is for first creature, second for second and so on.
+    /// When all creatures make moves, 
+    /// next move is again for first creature and so on.
+    /// </summary>
+    public string Moves { get; }
+
+    /// <summary>
+    /// Has all moves been done?
+    /// </summary>
+    public bool Finished = false;
+
+    private int _currentTurn = 0;
+    /// <summary>
+    /// Creature which will be moving current turn.
+    /// </summary>
+    public Creature CurrentCreature => Creatures[_currentTurn % Creatures.Count];
+
+    /// <summary>
+    /// Lowercase name of direction which will be used in current turn.
+    /// </summary>
+    public string CurrentMoveName => Moves[_currentTurn % Moves.Length].ToString();
+
+    /// <summary>
+    /// Simulation constructor.
+    /// Throw errors:
+    /// if creatures' list is empty,
+    /// if number of creatures differs from 
+    /// number of starting positions.
+    /// </summary>
+    public Simulation(Map map, List<Creature> creatures,
+        List<Point> positions, string moves)
+    {
+        if (creatures == null || creatures.Count == 0)
+        {
+            throw new ArgumentException("The creatures list cannot be empty.");
+        }
+
+        if (positions == null || positions.Count != creatures.Count)
+        {
+            throw new ArgumentException("The number of positions must match the number of creatures.");
+        }
+
+        if (string.IsNullOrWhiteSpace(moves))
+        {
+            throw new ArgumentException("Moves cannot be empty or null.");
+        }
+
+        Map = map ?? throw new ArgumentNullException(nameof(map));
+        Creatures = creatures;
+        Positions = positions;
+        Moves = moves;
+        for (int i = 0; i < creatures.Count; i++)
+        {
+            Creatures[i].InitMapAndPosition(Map, Positions[i]);
+        }
+        Moves = DirectionParser.Parse(moves).ToString();
+    }
+
+    public void Turn()
+    {
+        if (Finished)
+        {
+            throw new InvalidOperationException("Simulation is already finished.");
+        }
+
+        CurrentCreature.Go(DirectionParser.Parse(CurrentMoveName)[0]);
+        _currentTurn++;
+
+        if (_currentTurn >= Moves.Length)
+        {
+            Finished = true;
+        }
+    }
+}
